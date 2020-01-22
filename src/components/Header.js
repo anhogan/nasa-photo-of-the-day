0beba48d97db2332ea6515ee5eac94f68999dbad
Amanda Hogan <ahogan@teamworks.com>
2020-01-22 11:21:36 -0500

    Header component content

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,11 @@ const Header = () => {
 
   return (
     <div>
-      
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1200px-NASA_logo.svg.png" alt="NASA Logo"></img>
+      <div>
+        <h1>NASA</h1>
+        <h2>Astronomy Photo of the Day</h2>
+      </div>
     </div>
   );
 };
