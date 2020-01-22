4afdb70ac0de9ce2895fdb05b0cd483e10614776
Amanda Hogan <ahogan@teamworks.com>
2020-01-22 11:36:48 -0500

    Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import './stylesheets/Header.css';
 
 const Header = () => {
 
   return (
-    <div>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1200px-NASA_logo.svg.png" alt="NASA Logo"></img>
-      <div>
+    <div className="header-container">
+      <img className="nasa-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/1200px-NASA_logo.svg.png" alt="NASA Logo"></img>
+      <div className="headers">
         <h1>NASA</h1>
         <h2>Astronomy Photo of the Day</h2>
       </div>
